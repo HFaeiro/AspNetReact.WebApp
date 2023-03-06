b6b990fe96de042a8964dbb8214091a30829abc7
Michael J Richardson II <mike1994r@gmail.com>
2023-03-06 14:00:05 -0500

    somehow this didn't get imported with git changes

--- React.Front/src/Components/EditVideosModal.js
+++ React.Front/src/Components/EditVideosModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, Button, Row, Col, Form, Container } from 'react-bootstrap'
+import { Modal, Button, Row, Col, Form, Container, FloatingLabel } from 'react-bootstrap'
 
 export class EditVideosModal extends Component {
     state = {
