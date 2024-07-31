0ba4c48c5847ce917d28f9e294d0c5e22aa6ee3c
Mike R <mike1994r@gmail.com>
2024-07-31 01:36:59 -0400

    can't forget video upload model has changes

--- TeamManiacs.Core/Models/VideoUpload.cs
+++ TeamManiacs.Core/Models/VideoUpload.cs
@@ -8,16 +8,44 @@ namespace TeamManiacs.Core.Models
 {
     public partial class VideoUpload
     {
+        public string uploadId       { get; set; }
+        public string videoDuration  { get; set; }
+        public string videoHeight    { get; set; }
+        public string videoWidth     { get; set; }
+        public string chunkCount     { get; set; }
+        public string chunkNumber    { get; set; }
+        public IFormFile file        { get; set; }
+
+    }
+    public partial class VideoBlob
+    {
+        public VideoBlob() { }
+       public VideoBlob(VideoUpload videoUpload) {
+            int outChunkCount = 0;
+            int outChunkNumber = 0;
+            int.TryParse(videoUpload.chunkCount, out outChunkCount);
+            int.TryParse(videoUpload.chunkNumber, out outChunkNumber);
+
+            if(!string.IsNullOrEmpty(videoUpload.uploadId) && videoUpload.uploadId.Length == 36)
+            {
+                uploadId = new Guid(videoUpload.uploadId);
+            }
+            chunkCount = outChunkCount;
+            chunkNumber = outChunkNumber;
+
+            using (var stream = new System.IO.MemoryStream())
+            {
+                videoUpload.file.CopyTo(stream);
+                file = stream.ToArray();
+            }
+        }
+
+
         [Key]
         public Guid? uploadId       { get; set; }
-        //public int videoDuration  { get; set; }
-        //public int videoHeight    { get; set; }
-        //public int videoWidth     { get; set; }
-        public int chunkCount     { get; set; }
-        public int chunkNumber    { get; set; }
-        [NotMapped]
-        public IFormFile file       { get; set; }
-        public byte[] fileBytes  { get; set; }
+        public int chunkCount { get; set; }
+        public int chunkNumber { get; set; }
+        public byte[] file  { get; set; }
 
     }
 
